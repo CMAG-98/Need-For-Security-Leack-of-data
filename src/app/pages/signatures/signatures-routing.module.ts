becd43c247bbffa8c7c11f85a978e39cbc3d6d24
Alex461538 <alexgranada581@gmail.com>
2025-06-09 19:24:16 -0500

    added guards for green routes

--- src/app/pages/signatures/signatures-routing.module.ts
+++ src/app/pages/signatures/signatures-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ListComponent } from './list/list.component';
 import { ManageComponent } from './manage/manage.component';
+import { AuthenticatedGuard } from 'src/app/guards/authenticated.guard';
 
 const routes: Routes = [
-    { path: 'list', component: ListComponent },
-    { path: 'create', component: ManageComponent },
-    { path: 'view/:id', component: ManageComponent },
-    { path: 'update/:id', component: ManageComponent }
+    { path: 'list', component: ListComponent, canActivate: [AuthenticatedGuard] },
+    { path: 'create', component: ManageComponent, canActivate: [AuthenticatedGuard] },
+    { path: 'view/:id', component: ManageComponent, canActivate: [AuthenticatedGuard] },
+    { path: 'update/:id', component: ManageComponent, canActivate: [AuthenticatedGuard] }
 ];
 
 @NgModule({
